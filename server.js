5954cf2df4872849038aa471e92effef9b75e77b
Gary Anderson <ganderson@seattleacademy.org>
2016-05-23 15:59:34 +0000

    update server for new demo

--- server.js
+++ server.js
@@ -14,22 +14,24 @@
      });
  }
  getData();
- setInterval(getData,100);
+ setInterval(getData, 100);
 
- // heading = (data.tiltHeading + Math.PI / 2) * 180 / Math.PI;
- // res.send(heading.toFixed(0));
 
-app.use(express.static("./public"))
- app.all('/latestdata', function(req, res) {
+
+ app.use(express.static("./public"))
+
+ app.all('/heading', function(req, res) {
      counter++;
      res.setHeader("Access-Control-Allow-Origin", "*");
-     res.send(JSON.stringify(latestdata));
+     heading = (latestdata.tiltHeading + Math.PI / 2) * 180 / Math.PI;
+     res.send(heading.toFixed(0));
+     //res.send(JSON.stringify(latestdata));
  });
 
  app.all('/all', function(req, res) {
      counter++;
      res.setHeader("Access-Control-Allow-Origin", "*");
-     res.send(counter.toString());
+     res.send(JSON.stringify(latestdata));
  });
  app.all('/counter', function(req, res) {
      counter++;
